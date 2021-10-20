d6a77598df3cb8af5f3e0b4bdd8d83b26422389e
fofito29 <estefanyclara058@hotmail.com>
2021-10-20 15:36:51 -0300

    Rama hasta la parte 3 del ejercicio

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using CompAndDel.Pipes;
 using CompAndDel.Filters;
+using TwitterUCU; 
 
 namespace CompAndDel
 {
@@ -8,6 +9,31 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider(); 
+            IPicture picture= provider.GetPicture(@"C:\Users\estef\OneDrive - Universidad Católica del Uruguay\Documentos\UCU\SEGUNDO SEMESTRE\PROGRAMACION 2\Pipes_filters_start\EjercicioPipes_Filters_Start\src\Program\beer.jpg"); 
+            
+            //Creo primero los pipes del ultimo al primero 
+            
+            IFilter filtroGris= new FilterGreyscale();
+            IFilter filtroNegativo= new FilterNegative();
+            IFilter filtroAzul= new FilterBlurConvolution(); 
+
+            PipeNull pipeNull= new PipeNull(); 
+            PipeSerial pipeTwo= new PipeSerial(filtroNegativo, pipeNull); 
+            picture= pipeTwo.Send(picture);  
+            provider.SavePicture(picture, @"C:\Users\estef\OneDrive - Universidad Católica del Uruguay\Documentos\UCU\SEGUNDO SEMESTRE\PROGRAMACION 2\Pipes_filters_start\EjercicioPipes_Filters_Start\src\Program\beer1.jpg");
+            
+            var twitter= new TwitterImage(); 
+            Console.WriteLine(twitter.PublishToTwitter("ProbandoEC", @"C:\Users\estef\OneDrive - Universidad Católica del Uruguay\Documentos\UCU\SEGUNDO SEMESTRE\PROGRAMACION 2\Pipes_filters_start\EjercicioPipes_Filters_Start\src\Program\beer1.jpg")); 
+            
+            PipeSerial pipeOne= new PipeSerial(filtroGris, pipeTwo);
+            picture= pipeOne.Send(picture);            
+            provider.SavePicture(picture, @"C:\Users\estef\OneDrive - Universidad Católica del Uruguay\Documentos\UCU\SEGUNDO SEMESTRE\PROGRAMACION 2\Pipes_filters_start\EjercicioPipes_Filters_Start\src\Program\beer2.jpg"); 
+            Console.WriteLine(twitter.PublishToTwitter("Probando2EC", @"C:\Users\estef\OneDrive - Universidad Católica del Uruguay\Documentos\UCU\SEGUNDO SEMESTRE\PROGRAMACION 2\Pipes_filters_start\EjercicioPipes_Filters_Start\src\Program\beer2.jpg")); 
+
+            
+        
+        
         }
     }
 }
